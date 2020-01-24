6ff7c36282f4c719fc34ffaf477a023360a98be7
Md Abul Hossain <mdabulhossain780@gmail.com>
2020-01-24 22:06:10 +0600

    style added to table

--- src/components/UserTable.vue
+++ src/components/UserTable.vue
@@ -1,24 +1,27 @@
 <template>
-  <div class="container" id="userTable">
-    <div class="row">
-      <div class="col-sm-10 mx-auto text-center">
-        <div class="table-responsive">
-          <table class="table">
-            <thead>
-              <tr>
-                <th v-for="(header,i) in getHeader" :key="i">{{header.label}}</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="user in users" :key="user.id">
-                <td>{{ user.name }}</td>
-                <td>{{ user.email }}</td>
-                <td>{{ user.ocupation.toString() }}</td>
-                <td>{{ user.status }}</td>
-                <td>{{ user.internal_status }}</td>
-              </tr>
-            </tbody>
-          </table>
+  <div class="jumbotron">
+    <div class="container" id="userTable">
+      <div class="row">
+        <div class="col-sm-10 mx-auto text-center">
+          <div class="table-responsive">
+            <table class="table shadow">
+              <caption>User List</caption>
+              <thead class="bg-secondary text-white">
+                <tr>
+                  <th v-for="(header,i) in getHeader" :key="i">{{header.label}}</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="user in users" :key="user.id" class="bg-white">
+                  <td>{{ user.name }}</td>
+                  <td>{{ user.email }}</td>
+                  <td>{{ user.ocupation.toString() }}</td>
+                  <td>{{ user.status }}</td>
+                  <td>{{ user.internal_status }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
